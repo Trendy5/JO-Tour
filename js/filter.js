1f1aaf70b278380a36171c96086b1e9845370dbf
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-03-07 15:21:24 +0200

    filter cost

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,82 @@
 'use strict';
-var filtered=[];
-console.log(places)
+var filtered = [...Storge.places, ...Storge.restaurants];
+// console.log(filtered);
+function filterLoction(arrId) {
+  var arr = [];
+  for (let i = 0; i < filtered.length; i++) {
+    for (let j = 0; j < arrId.length; j++) {
+      if (filtered[i].id === arrId[j]) {
+        arr.push(filtered[i]);
+      }
+    }
+  }
+  if (arr.length > 0) {
+    filtered = arr;
+  }
+}
+// filterLoction([0]);
+// console.log(filtered);
+function filterType(arrType) {
+  var arr = [];
+  for (let i = 0; i < filtered.length; i++) {
+    for (let j = 0; j < arrType.length; j++) {
+      if (filtered[i].type === arrType[j]) {
+        arr.push(filtered[i]);
+      }
+    }
+  }
+  if (arr.length > 0) {
+    filtered = arr;
+  }
+}
+// filterType(['restaurants'])
+// console.log(filtered)
 
+function filterRate(rate) {
+  var arr = [];
+  var avgRate = 0;
+  for (let i = 0; i < filtered.length; i++) {
+    for (let j = 0; j < filtered[i].rate.length; j++) {
+      avgRate += filtered[i].rate[j].rate;
+    }
+    avgRate = avgRate / filtered[i].rate.length;
+    console.log(Math.floor(avgRate));
+    if (Math.floor(avgRate) >= rate) {
+      arr.push(filtered[i]);
+    }
+    avgRate = 0;
+  }
+  if (arr.length > 0) {
+    filtered = arr;
+  }
+}
+// filterRate(5);
+console.log(filtered);
+
+function filterCost(cost) {
+  var arr = [];
+  var avgCost = 0;
+  for (let i = 0; i < filtered.length; i++) {
+    console.log(filtered[i]);
+    if (filtered[i].avarge) {
+      for (let j = 0; j < filtered[i].avarge.length; j++) {
+        avgCost += filtered[i].avarge[j].avg;
+      }
+    } else {
+      arr.push(filtered[i]);
+      continue;
+    }
+    if (avgCost / filtered[i].avarge.length <= cost) {
+      console.log('jkljklj');
+      arr.push(filtered[i]);
+    }
+  }
+
+  if (arr.length > 0) {
+    // console.log('if')
+    filtered = arr;
+  }
+}
+
+filterCost(21);
+console.log(filtered);
--- js/storge.js
+++ js/storge.js
@@ -1,8 +1,10 @@
 'use strict';
+var Storge = {
+    locations: {'Amman':0,'Maan':1},
+    restaurants : [],
+    places : []
+}
 
-var locations = {'Amman':0,'Maan':1}
-var restaurants = []
-var places = []
 var Pleace = function(id,name,des,imgs,location,type){
     this.id=id,
     this.name = name,
@@ -12,7 +14,7 @@ var Pleace = function(id,name,des,imgs,location,type){
     this.rate = [],
     this.comments = [],
     this.type = type
-    places.push(this)
+    Storge.places.push(this)
 } 
 var Restrunt = function(id,name,des,imgs,location,type){
     this.id = id,
@@ -24,7 +26,7 @@ var Restrunt = function(id,name,des,imgs,location,type){
     this.comments = [],
     this.avarge= [],
     this.type = type
-    restaurants.push(this)
+    Storge.restaurants.push(this)
 }
 var Comment = function(object,user_name,comment,time){
     this.user_name = user_name,
@@ -37,18 +39,43 @@ var Rate = function(object,user_name,rate){
     this.rate = rate,
     object.rate.push(this)
 }
+var Avarge = function(object,user_name,avg){
+    this.user_name = user_name,
+    this.avg = avg,
+    object.avarge.push(this)
+}
 new Pleace(0,'Amman Citadel','ksldjflkjdslfljdslkjads',['img/DSC_0337-min.JPG',],'Center Downtown Amman','places')
 new Pleace(0,'The Duke\'s Diwan','ksldjflkjdslfljdslkjads',['img/DSC_1227-min.jpg'],'Downtown','places')
 new Pleace(0,'Al Kalha Stakirs','ksldjflkjdslfljdslkjads',['img/IMG_1793-min.jpg'],'Between Jabal Al Lweibdah and Downtown','places')
 new Pleace(0,'Downtown','ksldjflkjdslfljdslkjads',['img/Snapchat-1589789031-min.jpg'],'wastbald','places')
 new Pleace(1,'Petra','ksldjflkjdslfljdslkjads',['img/IMG_3960-min.jpg'],'wastbald','places')
-new Comment(places[0],'yazan','jlkjsdlkj','kldsjfkl')
+new Restrunt(1,'Abu zagla','ksldjflkjdslfljdslkjads',['img/IMG_3960-min.jpg'],'wastbald','restaurants')
+new Restrunt(1,'KFC','ksldjflkjdslfljdslkjads',['img/IMG_3960-min.jpg'],'wastbald','restaurants')
+new Comment(Storge.places[0],'yazan','jlkjsdlkj','kldsjfkl')
+new Rate(Storge.places[0],'yazan',10)
+new Rate(Storge.places[0],'yazan',10)
+new Rate(Storge.places[0],'yazan',4)
+new Rate(Storge.places[0],'yazan',6)
+new Rate(Storge.places[0],'yazan',4)
+new Rate(Storge.places[1],'yazan',2)
+new Rate(Storge.places[1],'yazan',10)
+new Rate(Storge.places[1],'yazan',10)
+new Rate(Storge.places[1],'yazan',3)
+new Rate(Storge.places[1],'yazan',6)
+new Rate(Storge.places[1],'yazan',2)
+new Rate(Storge.places[2],'yazan',10)
+new Rate(Storge.places[2],'yazan',10)
+new Rate(Storge.places[2],'yazan',10)
+new Rate(Storge.places[2],'yazan',4)
+new Rate(Storge.places[2],'yazan',0)
+new Rate(Storge.places[2],'yazan',0)
+new Rate(Storge.places[2],'yazan',0)
+new Avarge(Storge.restaurants[0],'yazan',20)
 if(!(localStorage.places)&&!(localStorage.restaurants)&&!(localStorage.locations)){
-localStorage.locations = JSON.stringify(locations)
-localStorage.restaurants = JSON.stringify(restaurants) 
-localStorage.places = JSON.stringify(places) 
+localStorage.locations = JSON.stringify(Storge.locations)
+localStorage.restaurants = JSON.stringify(Storge.restaurants) 
+localStorage.places = JSON.stringify(Storge.places) 
 }
-// console.log('dsf')
-locations= JSON.parse(localStorage.locations)
-restaurants = JSON.parse(localStorage.restaurants)
-places = JSON.parse(localStorage.places)
+Storge.locations= JSON.parse(localStorage.locations)
+Storge.restaurants = JSON.parse(localStorage.restaurants)
+Storge.places = JSON.parse(localStorage.places)
