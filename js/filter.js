c5847c16f3ae66a3a6beafe62c5fe81b739f05a7
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-03-07 22:09:00 +0200

    have values from form

--- js/filter.js
+++ js/filter.js
@@ -1,11 +1,11 @@
 'use strict';
-var filtered = [...Storge.places, ...Storge.restaurants];
+var filtered = [...Storge.places];
 // console.log(filtered);
 function filterLoction(arrId) {
   var arr = [];
   for (let i = 0; i < filtered.length; i++) {
     for (let j = 0; j < arrId.length; j++) {
-      if (filtered[i].id === arrId[j]) {
+      if (filtered[i].id == arrId[j]) {
         arr.push(filtered[i]);
       }
     }
@@ -40,7 +40,7 @@ function filterRate(rate) {
       avgRate += filtered[i].rate[j].rate;
     }
     avgRate = avgRate / filtered[i].rate.length;
-    console.log(Math.floor(avgRate));
+    // console.log(Math.floor(avgRate));
     if (Math.floor(avgRate) >= rate) {
       arr.push(filtered[i]);
     }
@@ -51,13 +51,13 @@ function filterRate(rate) {
   }
 }
 // filterRate(5);
-console.log(filtered);
+// console.log(filtered);
 
 function filterCost(cost) {
   var arr = [];
   var avgCost = 0;
   for (let i = 0; i < filtered.length; i++) {
-    console.log(filtered[i]);
+    // console.log(filtered[i]);
     if (filtered[i].avarge) {
       for (let j = 0; j < filtered[i].avarge.length; j++) {
         avgCost += filtered[i].avarge[j].avg;
@@ -73,10 +73,50 @@ function filterCost(cost) {
   }
 
   if (arr.length > 0) {
-    filtered = arr;
-  }
+alert('sdf')  }
 }
 
-filterCost(28);
-console.log(filtered);
+// filterCost(28);
 var filter = document.getElementById('filter');
+
+filter.addEventListener('submit', funFilter);
+
+function funFilter(event) {
+  event.preventDefault();
+  filtered = [...Storge.places];
+  var search = getCheckedCheckboxesFor('loction');
+  if (search.length > 0) {
+    filterLoction(search);
+    console.log(filtered);
+  }
+  // console.log(search);
+  search = getCheckedCheckboxesFor('place');
+  if(search.length > 0){
+    filterType(search)
+    console.log(filtered)
+  }
+  // console.log(search);
+  filterRate(event.target.rating.value)
+  search = event.target.Budget.value
+  if(search){
+    filterCost(search)
+    console.log(filtered)
+  }
+  // console.log(filtered)
+    // console.log(event.target.rating.value)
+    // console.log(event.target.Budget.value==false)
+  //  console.log(getCheckedCheckboxesFor('loction'))
+  //  console.log(getCheckedCheckboxesFor('place'))
+}
+
+function getCheckedCheckboxesFor(checkboxName) {
+  var checkboxes = document.querySelectorAll(
+      'input[name="' + checkboxName + '"]:checked'
+    ),
+    values = [];
+  Array.prototype.forEach.call(checkboxes, function(el) {
+    values.push(el.id);
+  });
+  return values;
+}
+console.log(filtered);
