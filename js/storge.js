aaa765280c9c4339aa7bdd116ea7cabf636d2aba
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-03-07 10:52:03 +0200

    bulid storge

--- js/storge.js
+++ js/storge.js
@@ -1,33 +1,44 @@
 'use strict';
 
-var loctoins = {'Amman':0,'Mafraq':1}
-var restrunt = []
-var pleaces = []
-var Pleace = function(id,name,des,imgs,loctoin){
+var locations = {'Amman':0,'Maan':1}
+var restaurants = []
+var places = []
+var Pleace = function(id,name,des,imgs,location){
     this.id=id,
     this.name = name,
     this.des = des,
     this.imgs = imgs,
-    this.loctoin =loctoin,
+    this.location =location,
     this.rate = [],
     this.comments = []
-    pleaces.push(this)
+    places.push(this)
 } 
-var Restrunt = function(id,name,des,imgs,loctoin){
+var Restrunt = function(id,name,des,imgs,location){
     this.id = id,
     this.name = name,
     this.descrption =des,
     this.imgs = imgs,
-    this.loctoin = loctoin
+    this.location = location
     this.rate = [],
     this.comments = [],
     this.avarge= [],
-    restrunt.push(this)
+    restaurants.push(this)
 }
-new Restrunt(0,'abujbarah','ksldjflkjdslfljdslkjads',['sdfafas'],'wastbald')
-new Restrunt(0,'abujbarah','ksldjflkjdslfljdslkjads',['sdfafas'],'wastbald')
-new Restrunt(0,'abujbarah','ksldjflkjdslfljdslkjads',['sdfafas'],'wastbald')
-
-localStorage.loctoins = JSON.stringify(loctoins)
-localStorage.restrunt = JSON.stringify(restrunt) 
-localStorage.pleaces = JSON.stringify(pleaces) 
+var Comment = function(object,user_name,comment,time){
+    this.user_name = user_name,
+    this.comment = comment,
+    this.time=time,
+    object.comments.push(this)
+}
+var Rate = function(object,user_name,rate){
+    this.user_name = user_name,
+    this.rate = rate,
+    object.rate.push(this)
+}
+// new Restrunt(0,'abujbarah','ksldjflkjdslfljdslkjads',['sdfafas'],'wastbald')
+// new Restrunt(0,'abujbarah','ksldjflkjdslfljdslkjads',['sdfafas'],'wastbald')
+// new Restrunt(0,'abujbarah','ksldjflkjdslfljdslkjads',['sdfafas'],'wastbald')
+// new Comment(restaurants[0],'yazan','jlkjsdlkj','kldsjfkl')
+localStorage.locations = JSON.stringify(locations)
+localStorage.restaurants = JSON.stringify(restaurants) 
+localStorage.places = JSON.stringify(places) 
