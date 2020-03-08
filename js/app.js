46fe9e4639ed746fde0b190becbb239a752dbaa7
Osama-Yousef <[osamawalidyousef@gmail.com]>
2020-03-08 15:13:08 +0200

    adding rate to local storage

--- js/app.js
+++ js/app.js
@@ -11,7 +11,37 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+
+
+
+
+function rating(name, user_name, rate) {
+
+  console.log(rate);
+  console.log(name);
+  console.log(user_name);
+  for (let index = 0; index < Storge.places.length; index++) {
+
+    if (name === Storge.places[index].name) {
+
+      new Rate(index, user_name, rate);
+      break;
+
+    }
+
+  }
+  localStorage.places = JSON.stringify(Storge.places);
+  for (let index = 0; index < filtered.length; index++) {
+    if (name === filtered[index].name) {
+      filtered[index].rate.push({ user_name: user_name, rate: rate });
+      console.log(filtered[index]);
+      break;
+    }
+  }
+}
+rating('KFC', 'BOSS', 5);
