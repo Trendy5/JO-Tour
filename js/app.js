b7885d8a2b18d84474ee22a7516b60a4e84af8ab
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-03-09 17:00:04 +0200

    work on gaming style

--- js/app.js
+++ js/app.js
@@ -111,3 +111,18 @@ function removePop(){
   var pop = document.getElementById('pop');
   pop.style.display = 'none'
 }
+
+function test3(){
+  console.log('jsdlkf')
+  var slides = document.getElementsByClassName('slideshow');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
